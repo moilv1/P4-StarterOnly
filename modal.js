b04bee2399527ff8824c4aea69e092ad4ef29bd8
Flahaut Melvin <mflahaut@auchan.fr>
2024-12-17 14:31:47 +0100

    errormessage + css navbar

--- modal.js
+++ modal.js
@@ -13,10 +13,25 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const closeBtn = document.querySelector(".close");
 const formulaire = document.querySelector(".formulaire");
 const landingModal = document.querySelector(".landingModal");
+const errorMessage = document.querySelectorAll(".errorMessage");
+
+
+// Value input
+const prenom = document.querySelector("input[name='prenom']");
+const nom = document.querySelector("input[name='nom']");
+const email = document.querySelector("input[name='email']");
+const dateNaissance = document.querySelector("input[name='birthdate']");
+const tournois = document.querySelector("input[name='tournois']");
+
+// REGEX
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // -> format email
+const dateRegex = /^\d{4}[-/]\d{2}[-/]\d{2}$/; // format aaaa/mm/jj
+const nomPrenomRegex = /^[a-zA-ZÀ-ÿ '-]+$/; // -> lettres, accents, espaces, apostrophes
 
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
 
 // launch modal form
 function launchModal() {
@@ -35,21 +50,51 @@ const landingModal = document.querySelector(".landingModal");
 function closeModal () {
   modalbg.style.display = "none";
 }
+
 closeBtn.addEventListener("click", closeModal);
+
+function validerFormulaire(event) {
+
+  // boucle pour reset display de errorMessage
+  for (let index = 0; index < errorMessage.length; index++) {
+    errorMessage[index].style.display= "none";
+  }
+
+  // Validation du prénom
+  if (!nomPrenomRegex.test(prenom.value)) {
+    errorMessage[0].style.display = "flex";
+    return;
+  }
+  
+  // Validation du nom
+  if (!nomPrenomRegex.test(nom.value)) {
+    errorMessage[1].style.display = "flex";
+    return;
+  }
+
+  // Validation de l'email
+  if (!emailRegex.test(email.value)) {
+    errorMessage[2].style.display = "flex";
+    return;
+  }
+  // Validation de la date de naissance
+  if (!dateRegex.test(dateNaissance.value)) {
+    errorMessage[3].style.display = "flex";
+    return;
+  }
+
+  // Validation du nombre de tournois
+  if (isNaN(tournois.value) || tournois.value < 0) {
+    errorMessage[4].style.display = "flex";
+    return;
+  }
+  formulaire.style.display = "none";
+  landingModal.style.display = "flex";
+}
 
 // Landing Page
 formulaire.addEventListener("submit", (event => {
   event.preventDefault();
-  formulaire.style.display = "none";
-  landingModal.style.display = "flex";
+  validerFormulaire();
+  console.log(dateNaissance.value);
 }));
-
-
-
-
-
-
-
-
-
-
