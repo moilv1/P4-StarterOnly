7589de9d2dd0acb49d6604086e7b6e0c6b7b651c
Flahaut Melvin <mflahaut@auchan.fr>
2025-01-23 18:24:29 +0100

    modification verification de champs prenom nom email

--- modal.js
+++ modal.js
@@ -31,8 +31,16 @@ const checkBoxCU = document.getElementById('checkbox1');
 // REGEX
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // -> format email
 const dateRegex = /^\d{4}[-/]\d{2}[-/]\d{2}$/; // format aaaa/mm/jj
-const nomPrenomRegex = /^[a-zA-ZÀ-ÿ '-]+$/; // -> lettres, accents, espaces, apostrophes
 
+prenom.addEventListener("blur", () => {
+  validerPrenom(prenom.value);
+})
+nom.addEventListener("blur", () => {
+  validerNom(nom.value)
+})
+email.addEventListener("blur", () => {
+  validerEmail(email.value)
+})
 
 // launch modal event
 
@@ -61,49 +69,30 @@ function closeModal () {
 
 closeBtn.addEventListener("click", closeModal);
 
-function validerFormulaire() {
-
-  // boucle pour reset display de errorMessage
-  for (let index = 0; index < errorMessage.length; index++) {
-    errorMessage[index].style.display= "none";
+function validerEmail(email) {
+  var regexMail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (regexMail.test(email)) {
+    errorMessage[2].style.display = 'block';
   }
-
-  // Validation du prénom
-  if (!nomPrenomRegex.test(prenom.value)) {
-    errorMessage[0].style.display = "flex";
-    return;
-  }
-  
-  // Validation du nom
-  if (!nomPrenomRegex.test(nom.value)) {
-    errorMessage[1].style.display = "flex";
-    return;
-  }
-
-  // Validation de l'email
-  if (!emailRegex.test(email.value)) {
-    errorMessage[2].style.display = "flex";
-    return;
-  }
-  // Validation de la date de naissance
-  if (!dateRegex.test(dateNaissance.value)) {
-    errorMessage[3].style.display = "flex";
-    return;
-  }
-
-  // Validation du nombre de tournois
-  if (isNaN(tournois.value) || tournois.value < 0) {
-    errorMessage[4].style.display = "flex";
-    return;
-  }
-
-  if (checkBoxCU.checked == false) {
-    errorMessage[5].style.display = "flex";
-    return;
-  }
-
-  formulaire.style.display = "none";
-  landingModal.style.display = "flex";
+  errorMessage[2].style.display = 'none';
+  return
 }
-
+function validerPrenom(name) {
+  if (name.length < 2) {
+    errorMessage[0].style.display= 'block';
+    return
+  }
+  errorMessage[0].style.display= 'none';
+  return
+  
+}
+function validerNom(name) {
+  if (name.length < 2) {
+    errorMessage[1].style.display= 'block';
+    return
+  }
+  errorMessage[1].style.display= 'none';
+  return
+  
+}
 
