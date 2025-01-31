f2a754be62b8dc25c7f29307bc485a4b3ffd9432
Melvin <melvinflahaut@gmail.com>
2025-01-31 09:54:55 +0100

    function valideremail OK ,validerBirthdate pas encore ok

--- modal.js
+++ modal.js
@@ -17,9 +17,6 @@ const closeBtn = document.querySelector(".close");
 const formulaire = document.querySelector(".formulaire");
 const landingModal = document.querySelector(".landingModal");
 const errorMessage = document.querySelectorAll(".errorMessage");
-const errorInput = document.querySelectorAll("input");
-console.log(errorInput);
-
 const input = document.querySelectorAll("input");
 
 
@@ -29,10 +26,10 @@ const nom = document.querySelector("input[name='nom']");
 const email = document.querySelector("input[name='email']");
 const dateNaissance = document.querySelector("input[name='birthdate']");
 const tournois = document.querySelector("input[name='tournois']");
-const boutonCoche = document.querySelector('input[name="location"]:checked');
-const boutonCocheCU = document.querySelector('input[id="checkbox1"]:checked');
+const checkBoxCU = document.getElementById('checkbox1');
 
 // REGEX
+const dateRegex = /^\d{4}[-/]\d{2}[-/]\d{2}$/; // format aaaa/mm/jj
 const patternEmail = new RegExp(email.pattern);
 const patternDate = new RegExp(birthdate.pattern);
 
@@ -59,120 +56,62 @@ closeBtn.addEventListener("click", closeModal);
 
 //-----------------------------------------//
 
-prenom.addEventListener("blur", () => {
+prenom.addEventListener("input", () => {
   validerPrenom(prenom.value);
 })
 
-nom.addEventListener("blur", () => {
+nom.addEventListener("input", () => {
   validerNom(nom.value)
 })
 
-email.addEventListener('change', function () {
-  const emailToConfirm = email.value
-  if (!validerEmail(emailToConfirm)) {
-    errorMessage[2].style.display = 'block';
-  } else {
-    errorMessage[2].style.display = 'none';
-  }
-   
+email.addEventListener('input', function () {
+  validerEmail(email.value)
 })
 
 dateNaissance.addEventListener('change', function() {
   const date = dateNaissance.value
   if (!validerDateNaissance(date)) {
     errorMessage[3].style.display = 'block';
+    console.log(dateNaissance.value);
+    console.log('err');
   } else {
     errorMessage[3].style.display = 'none';
-  }
-})
-
-tournois.addEventListener('input', function() {
-  validerTournois(tournois.value)
-})
-
-
-document.addEventListener('click', () => {
-  // Vérifie si un bouton radio est sélectionné
-  validerLocation(boutonCoche)
-  validerCU(boutonCocheCU)
-
-});
-
-
-
-
-
-
-
-/*function validerFormulaire(event) {
-
-  // boucle pour reset display de errorMessage
-  for (let index = 0; index < errorMessage.length; index++) {
-    errorMessage[index].style.display= "none";
-  }
-
-  // Validation du prénom
-  if (!nomPrenomRegex.test(prenom.value)) {
-    errorMessage[0].style.display = "flex";
-    return;
+    console.log('ok');
   }
   
-  // Validation du nom
-  if (!nomPrenomRegex.test(nom.value)) {
-    errorMessage[1].style.display = "flex";
-    return;
-  }
-
-  // Validation de l'email
-  if (!emailRegex.test(email.value)) {
-    errorMessage[2].style.display = "flex";
-    return;
-  }
-  // Validation de la date de naissance
-  if (!dateRegex.test(dateNaissance.value)) {
-    errorMessage[3].style.display = "flex";
-    return;
-  }
-
-  // Validation du nombre de tournois
-  if (isNaN(tournois.value) || tournois.value < 0) {
-    errorMessage[4].style.display = "flex";
-    return;
-  }
-  formulaire.style.display = "none";
-  landingModal.style.display = "flex";
-}*/
-
-
+})
 
 
 
 
 //-------------------------------------//
 
-function validerPrenom(prenom) {
-  console.log(prenom);
-  
-  if (!isNaN(prenom) || prenom.length < 2) {
-    errorMessage[0].style.display = 'block';
+function validerPrenom(name) {
+  if (name.length < 2) {
+    errorMessage[0].style.display= 'block';
     return;
   }
   errorMessage[0].style.display= 'none';
   return;
 }
+
 function validerNom(name) {
-  if (!isNaN(name) || name.length < 2) {
-    errorMessage[1].style.display = 'block';
+  if (name.length < 2) {
+    errorMessage[1].style.display= 'block';
     return;
   }
-  errorMessage[1].style.display = 'none';
+  errorMessage[1].style.display= 'none';
   return;
 }
 
 function validerEmail(email) {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  console.log(email);
-  return regex.test(email)
+  if (!patternEmail.test(email)) {
+    errorMessage[2].style.display = 'block';
+    return;
+  } else {
+    errorMessage[2].style.display = 'none';
+    return;
+  }
 }
 
 function validerDateNaissance(date) {
@@ -196,26 +135,4 @@ function validerDateNaissance(date) {
 
   return estValide
 }
-function validerTournois(int) {
-  if (isNaN(int) || int < 0) {
-    errorMessage[4].style.display = 'block';
-    return;
-  }
-  errorMessage[4].style.display = 'none';
-  return;
-}
 
-function validerLocation(location) {
-  if (location) {
-    errorMessage[5].style.display = 'none';
-    return;
-  }
-  errorMessage[5].style.display = 'block';
-}
-function validerCU(radioCU) {
-  if (radioCU) {
-    errorMessage[6].style.display = 'none';
-    return;
-  }
-  errorMessage[6].style.display = 'block';
-}
